8bbb9636e52d46e00c831fd63c5de4cc52aba4ae
Denis <denis.petrov56245@gmail.com>
2023-07-23 00:36:25 +0200

    prefinalcommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,7 +37,41 @@ public class Main {
 
         System.out.println(userLines);
         System.out.println(outputFile);
+
+        String filePath = "words.txt";
+        calculateWordFrequency(filePath);
+
     }
+    public static void calculateWordFrequency(String filePath) {
+        try {
+
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+
+
+            String content = String.join(" ", lines);
+
+
+            String[] words = content.split("\\s+");
+
+
+            Map<String, Integer> wordFrequency = new HashMap<>();
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+                }
+            }
+
+            List<Map.Entry<String, Integer>> sortedWords = new ArrayList<>(wordFrequency.entrySet());
+            sortedWords.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+
+            for (Map.Entry<String, Integer> entry : sortedWords) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}
 }
 
 
